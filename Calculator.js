6db33ed7959e3f0d2872476e8c7fd454d0f1b9cd
ThantPyae <thantpyaes01@gmail.com>
2024-11-09 17:03:50 +0630

    all feature added

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,7 @@
 let display = document.querySelector(".result");
-let preDisplay = document.querySelector(".pre-result");
+let preShowDisplay = document.querySelector(".pre-result");
+let calculationDone = false;
+let button =document.getElementById("number")
 
 function appendToDisplay(input) {
     display.innerHTML += input;
@@ -7,10 +9,13 @@ function appendToDisplay(input) {
 
 function clearDisplay() {
     display.innerHTML = "";
+    preShowDisplay.innerHTML = "";
 }
 
 function calculate() {
     try {
+        let preDisplay = display.innerHTML;
+        preShowDisplay.innerHTML = preDisplay + "=";
         display.innerHTML = eval(display.innerHTML);
     }
     catch (error) {
